a8db05e24d9a449d5487da10c257856cf1866e27
nilofulfarotuga-hue <nilofulfarotuga@gmail.com>
2026-02-27 13:42:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,4 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+  console.log("Server running on port " + 
